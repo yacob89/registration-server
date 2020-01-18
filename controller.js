38769c1a8ae865e2e91a95b90b49a82ca17e0f5e
Yacob Madiana <yacob.madiana@gmail.com>
2020-01-18 08:33:40 +0700

    Update create function

--- controller.js
+++ controller.js
@@ -13,6 +13,25 @@ exports.users = function(req, res) {
     });
 };
 
+exports.createUsers = function(req, res) {
+    
+    var phone_number = req.body.phone_number;
+    var first_name = req.body.first_name;
+    var last_name = req.body.last_name;
+    var gender = req.body.gender;
+    var email = req.body.email;
+
+    connection.query('INSERT INTO mitrais.registration (phone_number, first_name, last_name, birth_date, gender, email) VALUES (?, ?, ?, NOW(), ?, ?)',
+    [ phone_number ,first_name, last_name, gender, email ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok("Berhasil menambahkan user!", res)
+        }
+    });
+};
+
 exports.index = function(req, res) {
     response.ok("Hello from the Node JS RESTful side!", res)
 };
--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 module.exports = function(app) {
-    var todoList = require('./controller');
+  var todoList = require("./controller");
 
-    app.route('/')
-        .get(todoList.index);
+  app.route("/").get(todoList.index);
 
-    app.route('/users')
-        .get(todoList.users);
+  app.route("/users").get(todoList.users);
+
+  app.route("/users").post(todoList.createUsers);
 };
